bebe6e0db638fc11739e5e89e977bdff8003130a
Michael Aquilina <michael.aquilina@bizowie.com>
2014-09-11 20:01:32 +0000

    Fix bug where rate quotes were specified on 1lb

--- lib/Shipment/UPS.pm
+++ lib/Shipment/UPS.pm
@@ -360,6 +360,9 @@ sub _build_services {
   $shipto->{Phone}{Number} = $self->to_address->phone
      if $self->to_address->phone;
 
+  my $total_weight;
+  $total_weight += $_->weight for @{ $self->packages };
+
   my %services;
   try {
     $response = $interface->ProcessRate( 
@@ -388,7 +391,7 @@ sub _build_services {
               UnitOfMeasurement => {
                 Code => $units_type_map{$self->weight_unit} || $self->weight_unit,
               },
-              Weight => 1,
+              Weight => $total_weight,
             },
           },
         },
